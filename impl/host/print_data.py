c76a3541d8a4422baf28c2b45243e3a725861efb
Ransara <ransarawijitha@gmail.com>
2023-04-12 01:36:40 +0530

    Combined the two projects.
    Ethernet and FFT coprocessor works simultaneously.
    
     Changes to be committed:
    	new file:   impl/host/print_data.py
    	new file:   impl/sw/fft_core/Xilinx.spec
    	new file:   impl/sw/fft_core/cplx_data.c
    	new file:   impl/sw/fft_core/cplx_data.h
    	new file:   impl/sw/fft_core/dma_accel.c
    	new file:   impl/sw/fft_core/dma_accel.h
    	new file:   impl/sw/fft_core/fft.c
    	new file:   impl/sw/fft_core/fft.h
    	new file:   impl/sw/fft_core/helloworld.c
    	new file:   impl/sw/fft_core/lscript.ld
    	new file:   impl/sw/fft_core/platform.c
    	new file:   impl/sw/fft_core/platform.h
    	new file:   impl/sw/fft_core/platform_config.h
    	new file:   impl/sw/fft_core/stim.h
    	new file:   impl/sw/rsp/README.txt
    	new file:   impl/sw/rsp/Xilinx.spec
    	new file:   impl/sw/rsp/cplx_data.c
    	new file:   impl/sw/rsp/cplx_data.h
    	new file:   impl/sw/rsp/dma_accel.c
    	new file:   impl/sw/rsp/dma_accel.h
    	new file:   impl/sw/rsp/fft.c
    	new file:   impl/sw/rsp/fft.h
    	new file:   impl/sw/rsp/helloworld.c
    	new file:   impl/sw/rsp/lscript.ld
    	new file:   impl/sw/rsp/main.c
    	new file:   impl/sw/rsp/platform.c
    	new file:   impl/sw/rsp/platform.h
    	new file:   impl/sw/rsp/platform_config.h
    	new file:   impl/sw/rsp/platform_zynq.c
    	new file:   impl/sw/rsp/stim.h
    	new file:   impl/sw/rsp/udp_perf_client.c
    	new file:   impl/sw/rsp/udp_perf_client.h
    	new file:   impl/sw/udp_perf/README.txt
    	new file:   impl/sw/udp_perf/Xilinx.spec
    	new file:   impl/sw/udp_perf/i2c_access.c
    	new file:   impl/sw/udp_perf/iic_phyreset.c
    	new file:   impl/sw/udp_perf/lscript.ld
    	new file:   impl/sw/udp_perf/main.c
    	new file:   impl/sw/udp_perf/platform.c
    	new file:   impl/sw/udp_perf/platform.h
    	new file:   impl/sw/udp_perf/platform_config.h
    	new file:   impl/sw/udp_perf/platform_mb.c
    	new file:   impl/sw/udp_perf/platform_zynq.c
    	new file:   impl/sw/udp_perf/platform_zynqmp.c
    	new file:   impl/sw/udp_perf/sfp.c
    	new file:   impl/sw/udp_perf/si5324.c
    	new file:   impl/sw/udp_perf/udp_perf_client.c
    	new file:   impl/sw/udp_perf/udp_perf_client.h

---
+++ impl/host/print_data.py
@@ -0,0 +1,17 @@
+import socket
+
+UDP_IP = "192.168.1.100" # replace with your IP address
+UDP_PORT = 5001 # replace with your desired port number
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1440) # buffer size is 1024 bytes
+    print("Packet Start")
+    try:
+        print(data.decode('ascii')) # assumes received data is in UTF-8 format
+    except UnicodeDecodeError:
+        print(data.decode('latin-1')) # fallback to latin-1 encoding
+    print("Packet End")
