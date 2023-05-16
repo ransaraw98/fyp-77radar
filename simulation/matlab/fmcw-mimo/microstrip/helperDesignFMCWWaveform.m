b0ea76d4c783c0f30bfabc60df1fc5ccbff679a9
Ransara <ransarawijitha@gmail.com>
2023-05-16 18:14:06 +0530

    Radix-4 cyclic ramp  + FFT OK.
    Radix 4 fft config = 343 for conservative scaling.
    FFT confirmed with the data from matlab.
    Changes to be committed:
    	modified:   impl/proj_zuplus/ip_repo/SGEN_cyclic_1.0/component.xml
    	new file:   impl/sw/sgen_cyclic_zuplus/cyclic_gen.tcl
    	new file:   impl/sw/sgen_cyclic_zuplus/src/README.txt
    	new file:   impl/sw/sgen_cyclic_zuplus/src/helloworld.c
    	new file:   impl/sw/sgen_cyclic_zuplus/src/lscript.ld
    	exclude from build~
    	new file:   impl/sw/sgen_cyclic_zuplus/src/main.c
    	new file:   impl/sw/sgen_cyclic_zuplus/src/platform.h
    	new file:   impl/sw/sgen_cyclic_zuplus/src/platform_config.h
    	new file:   impl/sw/sgen_cyclic_zuplus/src/platform_zynqmp.c
    	new file:   impl/sw/sgen_cyclic_zuplus/src/signal_data.h
    	new file:   impl/sw/sgen_cyclic_zuplus/src/udp_perf_client.c
    	new file:   impl/sw/sgen_cyclic_zuplus/src/udp_perf_client.h
    	modified:   simulation/matlab/fmcw-mimo/microstrip/MIMORadarVirtualArrayExample.m
    	modified:   simulation/matlab/fmcw-mimo/microstrip/helperDesignFMCWWaveform.m

--- simulation/matlab/fmcw-mimo/microstrip/helperDesignFMCWWaveform.m
+++ simulation/matlab/fmcw-mimo/microstrip/helperDesignFMCWWaveform.m
@@ -4,14 +4,16 @@ function wav = helperDesignFMCWWaveform(c,lambda)
 
 % Copyright 2017 The MathWorks, Inc.
 
-range_max = 200; %300m target
+%sweeptime = (9.787 - 3.8)e-6;
+range_max = 150; %300m target
 %The sweep time can be computed based on the time needed for the signal to 
 % travel the unambiguous maximum range. In general, for an FMCW radar 
 % system, the sweep time should be at least five to six times the round 
 % trip time. This example uses a factor of 5.5.
-tm = 6*range2time(range_max,c);
+%tm = 6*range2time(range_max,c);
+tm = 6e-6;
 
-range_res =0.3;  %0.3 target
+range_res =14.0625;  %0.3 target
 bw = rangeres2bw(range_res,c);
 sweep_slope = bw/tm;
 fr_max = range2beat(range_max,sweep_slope,c);
